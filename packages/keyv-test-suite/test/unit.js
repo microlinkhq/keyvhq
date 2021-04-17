710fb45cbb5c4ce9de67ace1fd5a3464cc72878c
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-17 17:29:10 +0530

    Chore: Added CI and linked dependencies
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- packages/keyv-test-suite/test/unit.js
+++ packages/keyv-test-suite/test/unit.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const Keyv = require('keyv');
+const Keyv = require('@keyvhq/keyv');
 const keyvTestSuite = require('this');
 
 // KeyvOfficialTests(test, Keyv, 'sqlite://test/testdb.sqlite', 'sqlite://non/existent/database.sqlite');
