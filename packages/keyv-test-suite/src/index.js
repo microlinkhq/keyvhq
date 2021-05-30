d5b5577bf0bb151e0717a833ac8fae6cb5d1165f
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-05-30 21:01:04 +0200

    fix: typo keyvNamepsaceTests -> keyvNamepsaceTests

--- packages/keyv-test-suite/src/index.js
+++ packages/keyv-test-suite/src/index.js
@@ -1,19 +1,19 @@
 const keyvApiTests = require('./api.js');
 const keyvValueTests = require('./values.js');
-const keyvNamepsaceTests = require('./namespace.js');
+const keyvNamespaceTests = require('./namespace.js');
 const keyvIteratorTests = require('./iteration.js');
 
 const keyvTestSuite = (test, Keyv, store) => {
 	keyvIteratorTests(test, Keyv, store);
 	keyvApiTests(test, Keyv, store);
 	keyvValueTests(test, Keyv, store);
-	keyvNamepsaceTests(test, Keyv, store);
+	keyvNamespaceTests(test, Keyv, store);
 };
 
 Object.assign(keyvTestSuite, {
 	keyvApiTests,
 	keyvValueTests,
-	keyvNamepsaceTests,
+	keyvNamespaceTests,
 	keyvIteratorTests
 });
 
