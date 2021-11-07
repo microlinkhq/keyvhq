642689af193b69943c6d0d4f0b55f92419e3542e
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-11-07 18:40:15 +0530

    test: added type test for has
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- packages/core/test/keyv.test-d.ts
+++ packages/core/test/keyv.test-d.ts
@@ -40,6 +40,7 @@ new Keyv();
   expectType<string | undefined>(await keyv.get('foo')); // $ExpectType string | undefined
   expectType<string | undefined>(await keyv.get('foo', { raw: false })); // $ExpectType string | undefined
   expectType<DeserializedData<string> | undefined>(await keyv.get('foo', { raw: true })); // $ExpectType DeserializedData<string> | undefined
+  expectType<boolean>(await keyv.has('foo'))
   expectType<boolean>(await keyv.delete('foo')); // $ExpectType boolean
   expectType<void>(await keyv.clear()); // $ExpectType void
   expectType<AsyncIterator<[string, DeserializedData<string>]>>(keyv.iterator())
