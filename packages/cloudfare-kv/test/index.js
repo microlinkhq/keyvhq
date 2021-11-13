bfb8e89a7f2ee953a814934b837701364ef92632
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-11-13 22:47:20 +0100

    Update index.js

--- packages/cloudfare-kv/test/index.js
+++ packages/cloudfare-kv/test/index.js
@@ -6,7 +6,12 @@ const test = require('ava')
 
 const KeyvCFKV = require('..')
 
-const { key, email, accountId, namespaceId } = process.env
+const { 
+  CLOUDFLARE_KEY: key,
+  CLOUDFLARE_EMAIL: email,
+  CLOUDFLARE_ACCOUNT_ID: accountId,
+  CLOUDFLARE_ACCOUNT_ID: namespaceId
+} = process.env
 
 const store = () => new KeyvCFKV({ key, email, accountId, namespaceId })
 
