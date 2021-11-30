273cd6218417b9e131ad5393deb884ab1aaec7fa
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-11-30 10:40:29 +0530

    fix: removed dupe titles
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- packages/core/test/key.js
+++ packages/core/test/key.js
@@ -3,8 +3,8 @@
 const test = require('ava')
 const Keyv = require('../src')
 
-const createCase = (test, key, value = 'foobar') =>
-  test(String(key), async t => {
+const createCase = (test, key, value = 'foobar', testTitle) =>
+  test(testTitle || String(key), async t => {
     const store = new Map()
     const keyv = new Keyv({ store })
     await keyv.set(key, value)
@@ -21,4 +21,4 @@ createCase(test, true)
 createCase(test, false)
 createCase(test, '😍')
 createCase(test, '¯\\_(ツ)_/¯')
-createCase(test, Buffer.from('string'))
+createCase(test, Buffer.from('string'), 'foobar', 'string-buffer')
