be53d84c0c385524f29b887e3a9b326624566d08
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-01-29 10:40:58 +0100

    ci: better mysql setup

--- packages/mysql/test/index.js
+++ packages/mysql/test/index.js
@@ -6,6 +6,6 @@ const test = require('ava')
 
 const KeyvMysql = require('..')
 
-const dbUrl = process.env.MYSQL_URL || 'mysql://root:root@localhost/keyv_test'
+const dbUrl = process.env.MYSQL_URL || 'mysql://root:root@127.0.0.1/keyv_test'
 const store = () => new KeyvMysql(dbUrl)
 keyvTestSuite(test, Keyv, store)
