d3747127e89f354062729aed61db9380b6c042a6
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-24 22:40:44 +0100

    refactor: prefer import over require

--- packages/postgres/test/postgres.test-d.ts
+++ packages/postgres/test/postgres.test-d.ts
@@ -1,9 +1,7 @@
 /* eslint-disable no-new */
-/* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-invalid-void-type */
 
-import Keyv = require('@keyvhq/core')
-import KeyvPostgres = require('..')
+import Keyv from '@keyvhq/core'
+import KeyvPostgres from '..'
 
 new Keyv({ store: new KeyvPostgres({ uri: 'postgres://user:pass@localhost:5432/dbname', table: 'cache' }) })
 
--- packages/redis/test/redis.test-d.ts
+++ packages/redis/test/redis.test-d.ts
@@ -1,9 +1,7 @@
 /* eslint-disable no-new */
-/* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-invalid-void-type */
 
-import Keyv = require('@keyvhq/core')
-import KeyvRedis = require('..')
+import Keyv from '@keyvhq/core'
+import KeyvRedis from '..'
 
 new Keyv({
   store: new KeyvRedis({
