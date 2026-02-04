1174f0f355a7d0354984284a8b75c3dc22f211db
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-02-04 19:53:10 +0100

    test: add offline tests for specific connectors

--- packages/offline/test/keyv-redis.js
+++ packages/offline/test/keyv-redis.js
@@ -13,19 +13,39 @@ const keyvRedis = new Keyv({
   })
 })
 
-test('.set return false if store is unreachable', async t => {
+test('.set return false if redis store is unreachable', async t => {
   const keyv = keyvOffline(keyvRedis)
   const result = await keyv.set('foo', 'expires in 1 second', 1000)
   t.is(result, false)
 })
 
-test('.set return undefined if store is unreachable', async t => {
+test('.get return undefined if redis store is unreachable', async t => {
   const keyv = keyvOffline(keyvRedis)
   const result = await keyv.get('foo')
   t.is(result, undefined)
 })
 
-test('keep original keyv methods', async t => {
+test('keep original keyv methods with redis store', async t => {
   const keyv = keyvOffline(keyvRedis)
   t.is(typeof keyv.clear, 'function')
+  t.is(typeof keyv.delete, 'function')
+  t.is(typeof keyv.has, 'function')
+})
+
+test('does not throw when redis connection fails during set', async t => {
+  const keyv = keyvOffline(keyvRedis)
+  await t.notThrowsAsync(async () => {
+    await keyv.set('key1', 'value1')
+    await keyv.set('key2', 'value2')
+    await keyv.set('key3', 'value3')
+  })
+})
+
+test('does not throw when redis connection fails during get', async t => {
+  const keyv = keyvOffline(keyvRedis)
+  await t.notThrowsAsync(async () => {
+    await keyv.get('key1')
+    await keyv.get('key2')
+    await keyv.get('key3')
+  })
 })
